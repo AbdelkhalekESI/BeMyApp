3448f9269cac396cdd7023c1029b542638f23aaa
Zellat Abdelkhalek <ha_zellat@esi.dz>
2018-12-07 12:06:58 +0100

    add event routes

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
-
+var port = process.env.PORT || 3000 ;
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-mongoose.connect('mongodb://root:root00@ds227594.mlab.com:27594/bemyapp');
+mongoose.connect('mongodb://root:root00@ds227594.mlab.com:27594/bemyapp' , { useNewUrlParser: true } );
 var EventScheema = new mongoose.Schema({
     name : String ,
     Benevols  : Number ,
@@ -32,12 +32,56 @@ var EventScheema = new mongoose.Schema({
 }) ;
 var Event = mongoose.model("Event" , EventScheema ) ;
 
-Event.create({
+var UserScheema = new mongoose.Schema({
+    first_name : String ,
+    last_name  : String ,
+    EventLists : [EventScheema]
+}) ;
+var User = mongoose.model("User" , UserScheema ) ;
 
-} , () => console.log(""))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/" , function (req,res) {
+Event.find({} , function (err , Events) {
+    if(err) throw err ;
+    else res.send(Events) ;
+})
+})
+
+app.post("/newUser" , function (req,res) {
+    // username + userid + event id
+    Event.findById(req.query.eventId , function (err, foundedEvent) {
+        if(err) throw err ;
+        else {
+            foundedEvent.Benevols++ ;
+            foundedEvent.save();
+            User.findById(req.query.userId , function (err ,  foundedUser) {
+                if(err) throw err ;
+                else {
+                    foundedUser.EventLists.push(foundedEvent);
+                    foundedUser.save();
+                }
+            })
+        }
+    })
+    res.send("Done ! ") ;
+})
+
+app.post("/newEvent " , function (req,res) {
+    Event.create({
+    name : req.query.name ,
+        Benevols  : 0,
+        MaxBenevols : req.query.benevols ,
+        Lieu : req.query.lieu
+        ListeBesoins : ["Item1" , "Item2"],
+        EventsPictures : ["URL1" , "URL2"],
+        MainPicture : "URL"
+    } , () => console.log(""))
+
+
+})
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,4 +101,4 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen('3000' , () =>  console.log("server has started")) ;
+app.listen(port , () =>  console.log("server has started")) ;
