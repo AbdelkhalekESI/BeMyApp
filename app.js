1a78138bc974e5b4c3f034f553822d1a094950e9
Zellat Abdelkhalek <ha_zellat@esi.dz>
2018-12-07 02:59:13 +0100

    second

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +11,30 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://root:root00@ds227594.mlab.com:27594/bemyapp');
+var EventScheema = new mongoose.Schema({
+    name : String ,
+    Benevols  : Number ,
+    MaxBenevols : Number ,
+    Lieu : String ,
+    Date :  { type : Date , default : Date.now()} ,
+    ListeBesoins : [String],
+    EventsPictures : [String],
+    MainPicture : String
+}) ;
+var Event = mongoose.model("Event" , EventScheema ) ;
+
+Event.create({
+
+} , () => console.log(""))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -39,3 +56,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen('3000' , () =>  console.log("server has started")) ;
